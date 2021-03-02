e13e8824658285cc724ddec7aba0a44e4477ad30
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-03-02 16:35:40 +0100

    Reactivate models validation: first_name / last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :movies, foreign_key: :owner_id # enables user.movies or user.movies.bookings
 
-  # validates :last_name, presence: true
-  # validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
