a213488964394173595eb473e84523cb4a2e78fe
Valentin Faribeault <vfaribeault@outlook.com>
2021-03-04 12:19:46 +0100

    seeds OK and Homepage WIP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :index, :show]
 
   def home
+    @movies = Movie.where(release_year: (1990..1999))
+    @selection = @movies.first(6)
   end
 end
