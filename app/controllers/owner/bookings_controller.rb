d51cee951a7d80e0ee51df9c141ae781cf9e4ea6
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-03-05 14:45:47 +0100

    dashboard done

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -1,3 +1,23 @@
 class Owner::BookingsController < ApplicationController
+  def accept
+    @booking = Booking.find(params[:id])
+    @booking.status = "accepted"
 
+    if @booking.save
+      flash[:alert] = 'Request accepted!'
+      # when possible redirect to 'All requests'
+      redirect_to dashboard_path
+    end
+  end
+
+  def refuse
+    @booking = Booking.find(params[:id])
+    @booking.status = "rejected"
+
+    if @booking.save
+      flash[:alert] = 'Request rejected!'
+      # when possible redirect to 'All requests'
+      redirect_to dashboard_path
+    end
+  end
 end
